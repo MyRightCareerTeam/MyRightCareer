8255ee3177e805b79ca36a32edd07d31f55350b1
JSchuenke <JakeSchuenke@gmail.com>
2013-11-11 15:04:27 -0500

    added Content Manager Class

---
+++ SourceCode/MyRightCareerContent/ContentLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace MyRightCareerContent
+{
+    public class ContentLoader
+    {
+        private static string contentFolderLocation = @"C:\Program Files";
+        private Dictionary<string, XDocument> contents;
+
+        public ContentLoader()
+        {
+
+        }
+
+        public static List<Dictionary<string, string>> GetMenuButtons()
+        {
+            List<Dictionary<string, string>> menuButtons = new List<Dictionary<string, string>>();
+            
+            DirectoryInfo contentDirectory = new DirectoryInfo(contentFolderLocation);
+            List<string> subDirectories = new List<string>();
+            
+            foreach (DirectoryInfo subDirectory in contentDirectory.EnumerateDirectories())
+            {
+                subDirectories.Add(subDirectory.Name);
+                Console.WriteLine(subDirectory.Name);
+            }
+            
+            subDirectories.Sort();
+
+            return menuButtons;
+        }
+
+        public XDocument ParseXMLtoNode(String docPath)
+        {
+            return XDocument.Parse(docPath);   
+        }
+
+        public XDocument getPage(string pageName)
+        {
+            return this.contents[pageName];
+        }
+    }
+}
