549a43c26a6c58741f5403b9dd97811c9cc0c1af
JSchuenke <JakeSchuenke@gmail.com>
2013-12-04 20:49:37 -0500

    added text formatting to xml

--- SourceCode/MyRightCareer/MainPage.xaml.cs
+++ SourceCode/MyRightCareer/MainPage.xaml.cs
@@ -39,7 +39,47 @@ namespace MyRightCareer
 
         public void LoadPageContents(int exercise, int step)
         {
-            this.contentBlock.Text = this.content[exercise, step];
+            string text = this.content[exercise, step];
+            string[] boldText = text.Split(new string[] {"**"},StringSplitOptions.None);
+
+            this.contentBlock.Inlines.Clear();
+
+            for (int i = 0; i < boldText.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    string[] italicizedText = boldText[i].Split(new string[] { "#" }, StringSplitOptions.None);
+
+                    for (int j = 0; j < italicizedText.Length; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            string[] underlinedText = italicizedText[j].Split(new string[] { "__" }, StringSplitOptions.None);
+
+                            for (int k = 0; k < underlinedText.Length; k++)
+                            {
+                                if (k % 2 == 0)
+                                {
+                                    this.contentBlock.Inlines.Add(new Run() { Text = underlinedText[k]});
+                                }
+                                else
+                                {
+                                    this.contentBlock.Inlines.Add(new Run() { Text = underlinedText[k], TextDecorations = TextDecorations.Underline});
+                                }
+                            }
+                        }
+                        else
+                        {
+                            this.contentBlock.Inlines.Add(new Run() { Text = italicizedText[j], FontStyle = FontStyles.Italic });
+                        }
+                    }
+                }
+                else
+                {
+                    this.contentBlock.Inlines.Add(new Run() { Text = boldText[i], FontWeight = FontWeights.Bold });
+                } 
+            }
+ //           this.contentBlock.Text = text;
         }
     }
 }
